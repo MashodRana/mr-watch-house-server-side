39c633359201a206ddc75ad29805a1f0c071e14d
MashodRana <mashod0rana@gmail.com>
2021-11-11 21:44:10 +0600

    order api done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 // middle wire
@@ -10,35 +10,45 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2o6q0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-async function run(){
-    try{
-        await client.connect();
-        console.log("Database connection successful.");
-        const database = client.db('mr-watch-house');
-        const productsCollection = database.collection('products');
-
-        // Get all products
-        app.get('/products', async(req, res)=>{
-            const cursor = productsCollection.find({});
-            const products = await cursor.toArray();
-            return res.json(products);
-
-        });
-    }
-    finally{
-        // await client.close();
-    }
-}
-
-run()
-.catch(console.dir)
-
-app.get('/', (req, res)=>{
-    res.send('Welcome to the MR Watch House Server!!!');
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen(port, ()=>{
-    console.log('Server listening at port ', port);
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database connection successful.");
+    const database = client.db("mr-watch-house");
+    const productsCollection = database.collection("products");
+
+    // Get all products
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const products = await cursor.toArray();
+      return res.json(products);
+    });
+
+    const ordersCollection = database.collection("orders");
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      order.status = "pending";
+      const cursor = await ordersCollection.insertOne(order);
+      res.json({
+        message: "your order successfully placed. We will notify you soon.",
+      });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the MR Watch House Server!!!");
+});
+
+app.listen(port, () => {
+  console.log("Server listening at port ", port);
 });
