8fb657e6e9d200d13bcd06ef8dbe560b13d055d7
MashodRana <mashod0rana@gmail.com>
2021-11-14 00:04:15 +0600

    api writing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
+const { query } = require("express");
 const port = process.env.PORT || 5000;
 
 // middle wire
@@ -29,6 +30,35 @@ async function run() {
       return res.json(products);
     });
 
+    // Get a products
+    app.get("/products/:productId", async (req, res) => {
+      const productId = req.params?.productId;
+      const query = { _id: ObjectId(productId) };
+      const product = await productsCollection.findOne(query);
+      res.json(product);
+    })
+
+    // Add a new product
+    app.post('/products/add-product', async (req, res) => {
+      const product = req.body;
+      if (product) {
+        product.rating = 0;
+        const cursor = await productsCollection.insertOne(product);
+        res.json({ message: 'Product successfully added.' });
+      }
+
+    })
+
+    // Delete a prodcut
+    app.delete('/products/:productId', async (req, res) => {
+      const productId = req.params?.productId;
+      const query = { _id: ObjectId(productId) };
+      const cursor = await productsCollection.deleteOne(query);
+      res.json({ message: 'Product successfullly removed.' });
+    })
+
+    // -------------------- Order table ------------------
+
     const ordersCollection = database.collection("orders");
     // Place a order.
     app.post("/order", async (req, res) => {
@@ -41,15 +71,64 @@ async function run() {
       });
     });
 
-    app.get("/orders", async(req, res)=>{
+    // Cancel an order
+    app.delete("/order", async (req, res) => {
+      const orderId = req.body?.orderId;
+      const cursor = await ordersCollection.deleteOne({ _id: ObjectId(orderId) });
+      res.json({ message: 'Your order successfully canceled. Please visit our store to find your product.' });
+    });
+
+    // Get all orders for an email.
+    app.get("/orders", async (req, res) => {
       const email = req.query?.email;
-      if(email){
-        const query = {email:email};
+      if (email) {
+        const query = { email: email };
         const cursor = ordersCollection.find(query);
         const orders = await cursor.toArray();
         res.json(orders);
       }
+      else {
+        const cursor = ordersCollection.find({});
+        const orders = await cursor.toArray();
+        res.json(orders);
+
+      }
     });
+
+    // Update an order
+    app.put('/orders/:orderId', async (req, res) => {
+      const orderId = req.params?.orderId;
+      const query = { _id: ObjectId(orderId) };
+      const cursor = await ordersCollection.findOne(query);
+      let message = "";
+      if (cursor.status === 'shipped') {
+        message = "Already shipped!!!";
+      }
+      else {
+        const options = { $set: { status: 'shipped' } }
+        const cursor2 = await ordersCollection.updateOne(query, options);
+        message = "Product shipped."
+      }
+
+      res.json({ message: message });
+
+    });
+
+    //------------------- Reviews------------------------
+    const reviewCollections = database.collection('reviews');
+
+    // Get all reviews
+    app.get('/reviews', async(req, res)=>{
+      const reviews = await reviewCollections.find({}).toArray();
+      res.json(reviews);
+    })
+
+    // add a review
+    app.post('/review', async(req, res)=>{
+      const review = req.body;
+      const reviews = await reviewCollections.insertOne(review);
+      res.json({message: 'Your review successfully posted.'});
+    })
   } finally {
     // await client.close();
   }
